ead3004bbcba67a811b98f30eaf327fa662e8ca2
adarshrh <adarshrh004@gmail.com>
2020-03-05 00:39:29 -0600

    Implemented findEulerTour

--- src/axh190002/Euler.java
+++ src/axh190002/Euler.java
@@ -12,17 +12,14 @@ import axh190002.Graph.GraphAlgorithm;
 import axh190002.Graph.Factory;
 import axh190002.Graph.Timer;
 
-import java.util.Iterator;
+import java.util.*;
 import java.io.File;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.Scanner;
 
 
 public class Euler extends GraphAlgorithm<Euler.EulerVertex> {
     static int VERBOSE = 1;
     Vertex start;
-    List<Vertex> tour;
+    LinkedList<Vertex> tour;
     
 	// You need this if you want to store something at each node
     static class EulerVertex implements Factory {
@@ -58,33 +55,69 @@ public class Euler extends GraphAlgorithm<Euler.EulerVertex> {
                 return false;
             }
         }
-       DFS dfs = new DFS(this.g);
+    /*   DFS dfs = new DFS(this.g);
        //TODO implement stronglyConnectedComponents in DFS.java
        dfs = dfs.stronglyConnectedComponents(this.g);
        if(dfs.connectedComponents()!=1){
            System.out.println("Graph is not Eulerian");
            System.out.println("Graph is not strongly connected");
            return false;
-       }
+       }*/
         return true;
 	}
 
 
     public List<Vertex> findEulerTour() {
 	if(!isEulerian()) { return new LinkedList<>(); }
+        Stack<Vertex> stack = new Stack<>();
+	    stack.push(start);
+	    int totalEdges = this.g.m+1;
+        Edge e = this.g.adj(start).outEdges.get(0);
+        Vertex u = e.to;
+        e.setWeight(Integer.MIN_VALUE);
+	    while (!stack.empty() && totalEdges > 0){
+	        Vertex adjacent = getAdjacentVertex(u,this.g);
+	        if(null==adjacent){
+	            tour.addFirst(u);
+                totalEdges--;
+	            if(!stack.empty()){
+	                u = stack.pop();
+	            }
+            } else {
+	            stack.push(u);
+	            u = adjacent;
+            }
+
+        }
+	    while(!stack.empty() ) {
+	        tour.addFirst(stack.pop());
+        }
+	    tour.addFirst(start);
        // Graph is Eulerian...find the tour and return tour
 	return tour;
     }
-    
+
+    private Vertex getAdjacentVertex(Vertex u, Graph g) {
+        Vertex adjacent = null;
+        for (Edge e: g.adj(u).outEdges){
+            if(e.getWeight()!=Integer.MIN_VALUE){
+                adjacent = e.to;
+                e.setWeight(Integer.MIN_VALUE);
+                return adjacent;
+            }
+        }
+        return adjacent;
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner in;
-        if (args.length > 1) {
-            in = new Scanner(System.in);
+        if (args.length > 0) {
+            in =  new Scanner(new File(args[0]));
         } else {
-	    String input = "9 13 1 2 1 2 3 1 3 1 1 3 4 1 4 5 1 5 6 1 6 3 1 4 7 1 7 8 1 8 4 1 5 7 1 7 9 1 9 5 1";
+	    String input = "8 12 1 2 1 2 3 1 3 1 1 2 6 1 4 5 1 4 2 1 5 8 1 5 7 1 6 5 1 6 4 1 7 6 1 8 4 1";
             in = new Scanner(input);
         }
-	int start = 1;
+        int start = 1;
         if(args.length > 1) {
 	    start = Integer.parseInt(args[1]);
 		}
@@ -93,10 +126,10 @@ public class Euler extends GraphAlgorithm<Euler.EulerVertex> {
             VERBOSE = Integer.parseInt(args[2]);
         }
         Graph g = Graph.readDirectedGraph(in);
-	Vertex startVertex = g.getVertex(start);
+        Vertex startVertex = g.getVertex(start);
         Timer timer = new Timer();
 
-
+    System.out.println("Euler tour start");
 	Euler euler = new Euler(g, startVertex);
 	List<Vertex> tour = euler.findEulerTour();
 	
@@ -104,12 +137,16 @@ public class Euler extends GraphAlgorithm<Euler.EulerVertex> {
         timer.end();
         if(VERBOSE > 0) {
 	    System.out.println("Output:");
+	    for (Vertex v: tour){
+	        System.out.print(v.name+",");
+        }
             // print the tour as sequence of vertices (e.g., 3,4,6,5,2,5,1,3)
 	    System.out.println();
         }
         System.out.println(timer);
+        System.out.println("Size="+tour.size());
+        System.out.println("Start Vertex is "+euler.start.name);
 
-	
     }
 
     public void setVerbose(int ver) {
