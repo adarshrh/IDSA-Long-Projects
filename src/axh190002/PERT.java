126e1286c353eb4ea6988bf1495de9c8163c9d3c
Stewart Cannon <sjc160330@utdallas.edu>
2020-03-07 22:57:55 -0600

    adjusted imports to fit with library format

--- src/axh190002/PERT.java
+++ src/axh190002/PERT.java
@@ -6,10 +6,10 @@
 package axh190002;
 
 import axh190002.Graph.Vertex;
-import sjc160330.DFS;
-import sjc160330.Graph;
-import sjc160330.PERT;
-import sjc160330.PERT.PERTVertex;
+import axh190002.DFS;
+import axh190002.Graph;
+import axh190002.PERT;
+import axh190002.PERT.PERTVertex;
 import axh190002.Graph.Edge;
 import axh190002.Graph.GraphAlgorithm;
 import axh190002.Graph.Factory;
